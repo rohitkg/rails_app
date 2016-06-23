aeea923f361ad484c14313301c9b5f93c5bb06e0
rohit <rohit.gupta@dubaiairports.ae>
2016-06-23 14:52:44 +0400

    Cousera rest api

--- app/controllers/greeter_controller.rb
+++ app/controllers/greeter_controller.rb
@@ -1,11 +1,11 @@
 class GreeterController < ApplicationController
   def hello
-  	random_name = ["Rohit", "Sneha", "Veevek"]
-  	@name = random_name.sample
+  	random_names = ["Alex", "Joe", "Michael"]
+  	@name = random_names.sample
   	@time = Time.now
   	@times_displayed ||= 0
   	@times_displayed += 1
   end
-  def goodbye
-  end
 end
+
+
